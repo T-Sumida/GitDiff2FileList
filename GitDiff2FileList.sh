8316b819ba349b271dbd954629025725b3d3a506
Tomoyuki Sumida <ui.suto05@gmail.com>
2018-10-31 23:43:57 +0900

    edit sh

--- GitDiff2FileList.sh
+++ GitDiff2FileList.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
+
+# Setting Directory Path.
 DIR_LIST=(Logger
     NMF_for_Julia
 )
-
 
 for DIR in ${DIR_LIST[@]}; do
     # create diff patch
@@ -11,7 +12,7 @@ for DIR in ${DIR_LIST[@]}; do
     popd
 
     # diff patch 2 diff file list
-    for FILE in `grep "diff --git" $DIR.patch | awk -F '{print $4}'`; do
-        echo $FILE >> $DIR-list.txt
+    for FILE in `grep "diff --git" $DIR.patch | awk '{print $4}'`; do
+    echo $DIR/$FILE >> $DIR.list.txt
     done
 done
